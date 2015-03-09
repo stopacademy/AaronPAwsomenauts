ea8077b5238f482d940abfed73fc575db77ae3fd
Aaron Peron <aaron.peron@yahoo.com>
2015-03-09 14:19:31 -0700

    base

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,7 +12,11 @@ game.PlayerEntity = me.Entity.extend({
        }]);
    
        this.body.setVelocity(5, 20);
+       
+       this.renderable.addAnimation("idle", [78]);
+       this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
    
+       this.renderable.setCurrentAnimation("idle");
    },
    
    update: function(delta){
@@ -21,11 +25,23 @@ game.PlayerEntity = me.Entity.extend({
            //setVelocity() and multiplying it by me.timer.tick.
            //me.timer.tick makes the movement look smooth
            this.body.vel.x += this.body.accel.x * me.timer.tick;
+           this.flipX(true);
        }else{
            this.body.vel.x = 0;
        }
        
+       if(this.body.vel.x !== 0){
+       if(!this.renderable.isCurrentAnimation("walk")){
+           this.renderable.setCurrentAnimation("walk");
+       }
+   }else{
+       this.renderable.setCurrentAnimation("idle");
+   }
+       
+       
        this.body.update(delta);
+       
+       this._super(me.Entity, "update", [delta]);
        return true;
    }
 });
