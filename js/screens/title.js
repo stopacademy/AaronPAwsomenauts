65a755f15174243331d92e9b49ea99ebe418e38e
Aaron Peron <aaron.peron@yahoo.com>
2015-04-14 14:03:46 -0700

    exp2

--- js/screens/title.js
+++ js/screens/title.js
@@ -29,6 +29,7 @@ game.TitleScreen = me.ScreenObject.extend({
                         me.save.remove('exp2');
                         me.save.remove('exp3');
                         me.save.remove('exp4');
+                        me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
                         me.state.change(me.state.PLAY);
                     }
                 })));
@@ -54,7 +55,7 @@ game.TitleScreen = me.ScreenObject.extend({
                         game.data.exp3 = me.save.exp3;
                         game.data.exp4 = me.save.exp4;
                         me.input.registerPointerEvent('pointerdown', this);
-                        me.state.change(me.state.PLAY);
+                        me.state.change(me.state.SPENDEXP);
                     }
                 })));
         
